81674f2b88c42ecfdce6c6f39815313e11a3ca3e
bender <kudin.dima@gmail.com>
2026-06-04 14:11:04 +0000

    Update src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ import { Montserrat } from "next/font/google";
 
 
 export const metadata: Metadata = {
-  title: 'Cozy Beachfront Villa | Tropical Luxury in Zanzibar',
-  description: 'Escape to Cozy Beachfront Villa, an oceanfront haven in Kiwengwa, Zanzibar. Experience tropical luxury, private beach access, and authentic Swahili charm. Book your Tanzania vacation rental today!',
+  title: "Cozy Beachfront Villa | Tropical Luxury in Zanzibar",
+  description: "Located in Uroa, this villa is on the beach.\n\nBeit el-Sahel (Palace Museum) and House of Wonders are cultural highlights, and some of the area's activities can be experienced at Jozani Forest National Park Mangrove Walk and Kidichi Spice Farm. Siso Spice Farm and Kite Centre Zanzibar are also worth visiting. Our villa is a private property on the first line of the beach on the east coast. A quiet and cozy place for up to 16 people.6 rooms with ensuite bathroom, kitchen, spacious living room with pool table, tv, books, and an extra bathroom. Outdoor area with bar, pool, gazebos, and sun lounges. Until November/December 2023 the outdoor is shared with the guest of our adjacent property but privacy and quiet are ensured. Housekeeping and Security are included.",
   keywords: ["Zanzibar beach villa, Tanzania vacation rental, oceanfront stay Kiwengwa, luxury villa Zanzibar, beachfront rental, tropical paradise Zanzibar"],
   openGraph: {
     "title": "Cozy Beachfront Villa | Tropical Luxury in Zanzibar",
